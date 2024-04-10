cf3e4f9292e0fa988d74cd104b0256690deb7549
albertpinto <albertpinto123@gmail.com>
2024-04-10 13:18:11 -0700

    Focusing on the newsletter and built the first agent in the pipeline

--- Agents.py
+++ Agents.py
@@ -31,7 +31,9 @@ def main():
         human_input_mode="TERMINATE",
         max_consecutive_auto_reply=10,
         is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
-        code_execution_config={"work_dir": "/Users/albertpinto/Documents/work_dir"},
+        code_execution_config={"work_dir": "/home/albert/Documents/work_dir"
+                               ,"use_docker":False},
+        
         llm_config=llm_config,
         system_message="""
         Reply TERMINATE if the task has been solved to full satisfaction. 
@@ -46,21 +48,10 @@ def main():
     )
     agents = Agents(user_proxy=user_proxy, assistant=assistant, config_list=config_list, llm_config=llm_config)
 
-    # Define the task to be performed
-    task = """
-    Write a Python code that adds odd numbers from 1 to 1000 and prints out the result. 
-    After that, save the Python code in a file.
-    """
-
-    # Initiate chat with the assistant
-    agents.user_proxy.initiate_chat(
-        agents.assistant,
-        message=task,
-    )
     
-    # Define the task to be performed
+    # AI Topic Gneneration Task
     task = """
-    Summarize the contents of this URL https://arxiv.org/pdf/2403.15388.pdf into a text file.
+    Give me top 5 topics in AI that are trending in 2024 in this article https://www.techtarget.com/searchenterpriseai/tip/9-top-AI-and-machine-learning-trends and store it in a file named high_level_topics.txt.
     """
 
     # Initiate chat with the assistant
@@ -68,6 +59,18 @@ def main():
         agents.assistant,
         message=task,
     )
+
+    # AI Resource genration task
+    task = """
+    Print out the 5 topics list in the file high_level_topics.txt.
+    """
+
+    # Initiate chat with the assistant
+    agents.user_proxy.initiate_chat(
+        agents.assistant,
+        message=task,
+    )
+
 
 if __name__ == "__main__":
     main()          
